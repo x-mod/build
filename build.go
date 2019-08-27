47e7dfa94f43b9b686e0fea8364f644e07661452
JayL <liujianping.itech@qq.com>
2019-08-27 23:13:19 +0800

    rm unused func

--- build.go
+++ build.go
@@ -13,9 +13,3 @@ func String() string {
 	return fmt.Sprintf("%v, commit %v, built at %v", version, commit, date)
 }
 
-//Info build info
-func Info(v, m, d string) {
-	version = v
-	commit = m
-	date = d
-}
