9e6a855136664f50d43ee30b40c5c9ee37445708
JayL <liujianping.itech@qq.com>
2021-05-15 18:03:54 +0800

    add exports

--- build.go
+++ build.go
@@ -16,6 +16,21 @@ func String() string {
 	return fmt.Sprintf("%v, commit %v, built at %v", version, commit, date)
 }
 
+//String commit
+func Commit() string {
+	return commit
+}
+
+//String version
+func Date() string {
+	return date
+}
+
+//String version
+func Version() string {
+	return version
+}
+
 func init() {
 	date = time.Now().Format(time.RFC822Z)
 }
